c3e54860793a73646403158e815294319f4ff0ff
Joaoof <joaodeus400@gmail.com>
2024-01-26 10:04:48 -0300

    feat: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -33,13 +33,16 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       } // validação caso a senhas sejam diferentes
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
       })
 
-      return badRequest(new MissingParamError(''))
+      return {
+        statusCode: 200,
+        body: account
+      }
     } catch (error) {
       return serverError()
     }
